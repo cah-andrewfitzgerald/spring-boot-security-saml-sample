4cd82f54f95403522d5e43c7072f32bcdd218120
Fitzgerald, Andrew <andrew.fitzgerald@cardinalhealth.com>
2015-12-07 09:44:24 -0500

    add oauth deps + skeleton

---
+++ src/main/java/com/vdenotaris/spring/boot/security/saml/web/config/AuthServerConfig.java
@@ -0,0 +1,27 @@
+package com.vdenotaris.spring.boot.security.saml.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
+    }
+}
